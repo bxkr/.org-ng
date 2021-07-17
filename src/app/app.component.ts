845f335cff19cea11536805da17cd11ef15e67f9
Andrew <bxkr@mail.com>
2021-07-17 19:01:29 +0300

    Simple links and some preparations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'bxkr';
+  links = {
+    'GitHub': 'https://github.com/bxkr',
+    'VK': 'https://vk.com/qikel',
+    'last.fm': 'https://last.fm/user/blaxkr',
+    'Steam': 'https://steamcommunity.com/id/qikel/'
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {MatCardModule} from "@angular/material/card";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        MatCardModule,
+        BrowserAnimationsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
