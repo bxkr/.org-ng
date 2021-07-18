6c5bba7ec1c6b7d68e5372b2208a78f8f4937d08
Andrew <bxkr@mail.com>
2021-07-18 14:57:54 +0300

    Images instead of links, bg photo info

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {MatDialog} from "@angular/material/dialog";
+import {MatSnackBar} from "@angular/material/snack-bar";
 
 @Component({
   selector: 'app-root',
@@ -6,10 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(public dialog: MatDialog) {}
   links = {
-    'GitHub': 'https://github.com/bxkr',
-    'VK': 'https://vk.com/qikel',
-    'last.fm': 'https://last.fm/user/blaxkr',
-    'Steam': 'https://steamcommunity.com/id/qikel/'
+    'GitHub': ['https://github.com/bxkr', '../assets/github.svg'],
+    'VK': ['https://vk.com/qikel', '../assets/vk.svg'],
+    'last.fm': ['https://last.fm/user/blaxkr', '../assets/lastfm.svg'],
+    'Steam': ['https://steamcommunity.com/id/qikel/', '../assets/steam.svg']
+  }
+
+  openDialog(): void {
+    this.dialog.open(BackgroundPhotoDialog)
+  }
+}
+
+@Component({
+  selector: 'photo-dialog',
+  templateUrl: 'dialogs/photo.html',
+})
+export class BackgroundPhotoDialog {
+  constructor(public snackBar: MatSnackBar) {}
+  copied(): void {
+    this.snackBar.open('Скопировано в буфер обмена!', undefined, {
+      duration: 3000
+    })
   }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,20 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppComponent, BackgroundPhotoDialog} from './app.component';
 import {MatCardModule} from "@angular/material/card";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatIconModule} from "@angular/material/icon";
+import {MatButtonModule} from "@angular/material/button";
+import {MatDialogModule} from "@angular/material/dialog";
+import {ClipboardModule} from "ngx-clipboard";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundPhotoDialog
   ],
-    imports: [
-        BrowserModule,
-        AppRoutingModule,
-        MatCardModule,
-        BrowserAnimationsModule
-    ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    MatCardModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatButtonModule,
+    MatDialogModule,
+    MatSnackBarModule,
+    ClipboardModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
